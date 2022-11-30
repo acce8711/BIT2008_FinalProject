7dd2b5f4aaf47082f1ec51471d2f0f2dab416d2e
acce8711 <101413675+acce8711@users.noreply.github.com>
2022-11-30 17:05:26 -0500

    updated tables

--- insert.sql
+++ insert.sql
@@ -1,2 +1,16 @@
-/*Insert Data Here*/
-hello 
+/*Insert CLient Data Here*/
+INSERT INTO client (first_name, last_name, client_password) VALUES('Jim', 'Rocks', 'ILikeRocks');
+INSERT INTO client (first_name, last_name, client_password) VALUES('Chloe', 'Joyce', 'idopds762');
+INSERT INTO client (first_name, last_name, client_password) VALUES('Gavin', 'Morse', 'MorseCode9237');
+
+/*Insert Account Data Here*/
+INSERT INTO account (total_balance, account_type, num_cosigner) VALUES(130, 'savings', 3);
+INSERT INTO account (total_balance, account_type, num_cosigner) VALUES(3000, 'savings', 1);
+INSERT INTO account (total_balance, account_type, num_cosigner) VALUES(4500, 'checkings', 2);
+
+/*Insert account_client Data Here*/
+INSERT INTO client_account VALUES(1,2,TRUE,TRUE,TRUE);
+INSERT INTO client_account VALUES(1,3,FALSE,TRUE,FALSE);
+
+
+
--- tables.sql
+++ tables.sql
@@ -10,14 +10,14 @@ CREATE TABLE client(
 CREATE TABLE client_phone(
 	client_id INT,
 	phone_numbr	NUMERIC (10, 0) NOT NULL,
-	FOREIGN KEY(client_id) REFERENCES client(client_id),
+	FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE,
 	PRIMARY KEY(client_id)
 );
 
 CREATE TABLE client_address(
 	client_id INT,
 	address	VARCHAR (50) NOT NULL,
-	FOREIGN KEY(client_id) REFERENCES client(client_id),
+	FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE,
 	PRIMARY KEY(client_id)
 );
 
@@ -25,20 +25,23 @@ CREATE TABLE account(
 	account_id SERIAL,
 	total_balance NUMERIC(10) DEFAULT 0,	
 	account_type VARCHAR(20) DEFAULT 'savings' CHECK (account_type = 'savings' OR account_type = 'checkings'),
-	num_cosigner INT DEFAULT 0,
+	num_cosigner INT DEFAULT 1,
+	required_signatures INT DEFAULT 1 CHECK(required_signatures <= num_cosigner),
 	PRIMARY KEY(account_id)
 );
+
 
 CREATE TABLE client_account(
 	client_id INT,
 	account_id	INT,
-	sign_role BOOLEAN,
-	view_role BOOLEAN,
-	pay_role BOOLEAN,
-	FOREIGN KEY(client_id) REFERENCES client(client_id),
-	FOREIGN KEY(account_id) REFERENCES account(account_id),
+	sign_role BOOLEAN NOT NULL,
+	view_role BOOLEAN NOT NULL,
+	pay_role BOOLEAN NOT NULL,
+	FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE,
+	FOREIGN KEY(account_id) REFERENCES account (account_id) ON DELETE CASCADE ,
 	PRIMARY KEY(client_id, account_id)	
 );
+
 
 CREATE TABLE statements(
 	statement_id SERIAL,
@@ -46,20 +49,20 @@ CREATE TABLE statements(
 	source_account INT, 
 	initiator_client INT,
 	total_amount NUMERIC(10,0) DEFAULT 0,
-	min_signature INT DEFAULT 1,
 	confirmed BOOL DEFAULT FALSE,
 	payer INT,
-	FOREIGN KEY(source_account) REFERENCES account(account_id),
-	FOREIGN KEY(initiator_client) REFERENCES client(client_id),
-	FOREIGN KEY(payer) REFERENCES client(client_id),
+	FOREIGN KEY(source_account) REFERENCES account(account_id)ON DELETE CASCADE,
+	FOREIGN KEY(initiator_client) REFERENCES client(client_id)ON DELETE CASCADE ,
+	FOREIGN KEY(payer) REFERENCES client(client_id)ON DELETE CASCADE,
 	PRIMARY KEY(statement_id)
 );
 
 CREATE TABLE statement_signer(
 	statement_id INT,
 	client_id INT,
-	FOREIGN KEY(statement_id) REFERENCES statements(statement_id),
-	FOREIGN KEY(client_id) REFERENCES client(client_id),
+	sign BOOLEAN DEFAULT FALSE,
+	FOREIGN KEY(statement_id) REFERENCES statements(statement_id)ON DELETE CASCADE,
+	FOREIGN KEY(client_id) REFERENCES client(client_id)ON DELETE CASCADE,
 	PRIMARY KEY(statement_id, client_id)
 );
 
@@ -70,7 +73,7 @@ CREATE TABLE transactions(
 	transaction_time TIMESTAMP NOT NULL,
 	note VARCHAR(100) DEFAULT '',
 	transaction_to INT,
-	FOREIGN KEY(statement_id) REFERENCES statements(statement_id),
-	FOREIGN KEY(statement_id) REFERENCES statements(statement_id),
+	FOREIGN KEY(statement_id) REFERENCES statements(statement_id) ON DELETE CASCADE,
+	FOREIGN KEY(statement_id) REFERENCES statements(statement_id) ON DELETE CASCADE,
 	PRIMARY KEY(statement_id, amount, transaction_type, transaction_time, note, transaction_to)
 );
