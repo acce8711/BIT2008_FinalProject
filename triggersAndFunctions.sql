f15d5b2c3be9081ca6a25b1349b383f9f763e04b
acce8711 <101413675+acce8711@users.noreply.github.com>
2022-12-01 11:44:44 -0500

    added comments to triggers

--- triggersAndFunctions.sql
+++ triggersAndFunctions.sql
@@ -6,11 +6,13 @@ CREATE OR REPLACE FUNCTION check_if_confirmed_transactions()
 	RETURNS TRIGGER
 	AS $$
 	BEGIN
+	--Checking if the statement that the transactions is associated with is confirmed. If yes, then transaction will not be added to transactions and an error will be displayed to user
 	IF ((SELECT statements.confirmed 
 		FROM statements
 		WHERE statements.statement_id = NEW.statement_id
 	   ) = TRUE) THEN
 	   RAISE EXCEPTION 'transactions cannot be added, statement is not longer editable';
+	--id the statement that the transaction is associated with is not confirmed then a new row will be added to transactions table
 	ELSE
 		RETURN NEW;
 	END IF;
@@ -31,18 +33,20 @@ CREATE OR REPLACE FUNCTION set_statement_total()
 	RETURNS TRIGGER
 	AS $$
 	BEGIN
+	--i think this if statement needs to be removed (need to check)
 	IF (SELECT statements.confirmed 
 		FROM statements
 		WHERE statements.statement_id = NEW.statement_id
 	   ) = TRUE THEN
 	   RAISE NOTICE 'The Credit Card you have entered has expired.'
        ROLLBACK TRANSACTION
-	--checking if the transaction type is 'deposit'
+	-- if the transaction type is 'deposit' then transaction amount will be added to total statement amount.
 	IF (NEW.transaction_type = 'Deposit') THEN
 		UPDATE statements
 		SET total_amount = statements.total_amount + NEW.amount
 		WHERE statements.statement_id = NEW.statement_id;
 		RETURN NEW;
+	-- if the deposit type is 'withdrawal' then transaction amount will be deducted from total statement amount.
 	ELSE
 		UPDATE statements
 		SET total_amount = statements.total_amount - NEW.amount
