e7e41d2c26b66ae71d7c25d1c0ed4698b308b4e4
acce8711 <101413675+acce8711@users.noreply.github.com>
2022-12-03 22:22:07 -0500

    worked on triggers

--- new statement table update/NewTriggers.sql
+++ new statement table update/NewTriggers.sql
@@ -1,1 +1,272 @@
-f
+--Statement table triggers
+CREATE OR REPLACE FUNCTION statement_edit_delete()
+	RETURNS TRIGGER
+	AS $$
+	DECLARE signature_count INT;
+	BEGIN
+	
+    SELECT COUNT(*) INTO signature_count
+	FROM statement_signer
+	WHERE statement_signer.sign = TRUE AND statement_signer.statement_id = NEW.statement_id;
+	
+	--checking if the statement is already confirmed
+	IF ((SELECT statement_confirmation.confirmed
+		FROM statement_confirmation
+		WHERE statement_confirmation.statement_id = COALESCE(NEW.statement_id, OLD.statement_id)) = TRUE) THEN
+		RAISE EXCEPTION 'statement is confirmed. Cannot be deleted or edited';	
+	END IF;
+	
+	--checking if the trigger operation was UPDATE and the statement already has at least one signature
+	IF ( signature_count >= 1 AND TG_OP = 'UPDATE') THEN
+			RAISE EXCEPTION 'statement cannot be edited. There is already at least one signature';	
+	END IF;
+	
+	--Returning NEW or OLD depending on whether the trgger operation was UPDATE or DELETE
+	IF (TG_OP = 'UPDATE') THEN
+		RETURN NEW;
+	ELSE
+		RETURN OLD;
+	END IF;
+	
+	END;
+	$$
+	LANGUAGE plpgsql;
+
+CREATE TRIGGER edit_statement_trigger
+BEFORE UPDATE
+ON statements
+FOR EACH ROW
+EXECUTE PROCEDURE statement_edit_delete();
+
+CREATE TRIGGER delete_statement_trigger
+BEFORE DELETE
+ON statements
+FOR EACH ROW
+EXECUTE PROCEDURE statement_edit_delete();
+
+
+--Trigger that checks if the intitaor is associated with the statement source account
+CREATE OR REPLACE FUNCTION verify_initiator()
+	RETURNS TRIGGER
+	AS $$
+	BEGIN
+	
+	--checking if the initiator is associated to the statement account and has the payer role
+	IF (NEW.initiator_client IN (
+		SELECT client_account.client_id
+		FROM client_account
+		WHERE client_account.account_id = NEW.source_account
+	)) THEN
+	  	RETURN NEW;
+	ELSE
+		RAISE EXCEPTION 'invalid initiator.';
+	END IF;
+	
+	END;
+	$$
+	LANGUAGE plpgsql;
+
+CREATE TRIGGER verify_initiator_trigger
+BEFORE INSERT
+ON statements
+FOR EACH ROW
+EXECUTE PROCEDURE verify_initiator();
+
+--Statement_confirmation table triggers
+
+--Trigger that checks if entered payer actually has pay role and is asscoiated with the statement source account
+CREATE OR REPLACE FUNCTION verify_payer()
+	RETURNS TRIGGER
+	AS $$
+	BEGIN
+	
+	--checking if the payer is associated to the statement source account and has the payer role
+	IF (NEW.payer_id IN (
+		SELECT client_account.client_id
+		FROM client_account
+		WHERE client_account.pay_role = TRUE 
+		AND client_account.account_id = (
+			SELECT statements.source_account
+			FROM statements
+			WHERE statements.statement_id = NEW.statement_id)
+	)) THEN
+	  	RETURN NEW;
+	ELSE
+		RAISE EXCEPTION 'invalid payer.';
+	END IF;
+	
+	END;
+	$$
+	LANGUAGE plpgsql;
+
+CREATE TRIGGER verify_payer_insert_trigger
+BEFORE INSERT
+ON statement_confirmation
+FOR EACH ROW
+EXECUTE PROCEDURE verify_payer();
+
+CREATE TRIGGER verify_payer_update_trigger
+BEFORE UPDATE
+ON statement_confirmation
+FOR EACH ROW
+EXECUTE PROCEDURE verify_payer();
+
+CREATE OR REPLACE FUNCTION confirm_statement()
+	RETURNS TRIGGER
+	AS $$
+	DECLARE signature_count INT;
+	BEGIN
+	
+    SELECT COUNT(*) INTO signature_count
+	FROM statement_signer
+	WHERE statement_signer.sign = TRUE AND statement_signer.statement_id = NEW.statement_id;
+	
+	--checking if the statement is already confirmed
+	IF (OLD.confirmed = TRUE) THEN
+		RAISE EXCEPTION 'statement is already confirmed.';
+	END IF;
+	
+	IF(signature_count <
+	  (SELECT account.required_signatures
+	  FROM account
+	  WHERE account_id = (SELECT statements.source_account
+						 FROM statements
+						 WHERE statements.statement_id = NEW.statement_id)
+	  ))THEN
+	 	RAISE EXCEPTION 'not enough signatures.';
+	END IF;
+	RETURN NEW;
+	END;
+	$$
+	LANGUAGE plpgsql;
+	
+CREATE TRIGGER confirm_statement_trigger
+BEFORE UPDATE
+ON statement_confirmation
+FOR EACH ROW
+EXECUTE PROCEDURE confirm_statement();
+
+CREATE TRIGGER confirm_statement_insert_trigger
+BEFORE INSERT
+ON statement_confirmation
+FOR EACH ROW
+EXECUTE PROCEDURE confirm_statement();
+
+
+--Trigger will update from and to accounts once statement is confirmed
+CREATE OR REPLACE FUNCTION update_account_balance()
+	RETURNS TRIGGER
+	AS $$
+	DECLARE from_account INT; total INT;
+	BEGIN
+	
+	SELECT statements.source_account INTO from_account
+	FROM statements
+	WHERE statements.statement_id = NEW.statement_id;
+	
+	SELECT statements.total_amount INTO total
+	FROM statements
+	WHERE statements.statement_id = NEW.statement_id;
+	
+	IF (NEW.confirmed = FALSE) THEN
+		RETURN NULL;
+   	END IF;
+	
+	UPDATE account
+	SET account.total_balance = account.total_balance + total
+	WHERE account.account_id = from_account;
+	RETURN NULL;
+	
+	END;
+	$$
+	LANGUAGE plpgsql;
+
+CREATE TRIGGER update_account_balance_trigger
+AFTER UPDATE
+ON statement_confirmation
+FOR EACH ROW
+EXECUTE PROCEDURE update_account_balance();
+
+--Statement_signer table triggers
+
+--Trigger verifes that an inserted signer is asssociated to the statemennt source account, has a sign role and the statement is not already cnfirmed
+CREATE OR REPLACE FUNCTION verify_signer()
+	RETURNS TRIGGER
+	AS $$
+	BEGIN
+	
+	IF (NEW.statement_id IN (
+		SELECT statement_confirmation.statement_id 
+		FROM statement_confirmation
+		WHERE statement_confirmation.confirmed = TRUE)) THEN
+		RAISE EXCEPTION 'signer cannot be added. statement can no longer be edited';
+	END IF;
+	
+	--checking if the client(signer) is associated with the statement source account and has sign role
+	IF(NEW.signer_id IN (
+		SELECT client_account.client_id
+		FROM client_account
+		WHERE client_account.sign_role = TRUE AND client_account.account_id = (
+			SELECT statements.source_account
+			FROM statements
+			WHERE statements.statement_id = NEW.statement_id))
+	 ) THEN
+	    RETURN NEW;
+	ELSE
+		RAISE EXCEPTION 'signer cannot be added. user does not have sign role.';
+	END IF;
+	
+	END;
+	$$
+	LANGUAGE plpgsql;
+
+CREATE TRIGGER verify_signer_trigger
+BEFORE INSERT
+ON statement_signer
+FOR EACH ROW
+EXECUTE PROCEDURE verify_signer();
+
+CREATE OR REPLACE FUNCTION sign_unsign()
+	RETURNS TRIGGER
+	AS $$
+	BEGIN
+	IF (NEW.statement_id IN (
+		SELECT statement_confirmation.statement_id 
+		FROM statement_confirmation
+		WHERE statement_confirmation.confirmed = TRUE)) THEN
+		RAISE EXCEPTION 'cannot sign/unsign. statement can no longer be edited';
+	END IF;
+	RETURN NEW;
+	END;
+	$$
+	LANGUAGE plpgsql;
+
+CREATE TRIGGER sign_unsign_trigger
+BEFORE UPDATE
+ON statement_signer
+FOR EACH ROW
+EXECUTE PROCEDURE sign_unsign();
+
+CREATE OR REPLACE FUNCTION remove_signer()
+	RETURNS TRIGGER
+	AS $$
+	BEGIN
+	IF (OLD.statement_id IN (
+		SELECT statement_confirmation.statement_id 
+		FROM statement_confirmation
+		WHERE statement_confirmation.confirmed = TRUE)) THEN
+		RAISE EXCEPTION 'signer could not be removed. statement can no longer be edited';
+	END IF;
+	RETURN OLD;
+	END;
+	$$
+	LANGUAGE plpgsql;
+
+CREATE TRIGGER remove_signer_trigger
+BEFORE DELETE
+ON statement_signer
+FOR EACH ROW
+EXECUTE PROCEDURE remove_signer();
+
+
+
--- new statement table update/insert.sql
+++ new statement table update/insert.sql
@@ -49,6 +49,9 @@ INSERT INTO client_account VALUES(9,5,TRUE,TRUE,TRUE);
 INSERT INTO statements (note, source_account, initiator_client) VALUES('my statement', 4, 2);
 INSERT INTO statements (note, source_account, initiator_client) VALUES('my statement', 5, 8);
 INSERT INTO statements (note, source_account, initiator_client) VALUES('my statement', 4, 8);
+INSERT INTO statements (note, source_account, initiator_client) VALUES('my statement', 4, 8);
+INSERT INTO statements (note, source_account, initiator_client) VALUES('my statement', 4, 1);
+INSERT INTO statements (note, source_account, initiator_client) VALUES('my statement', 4, 1);
 
 
 --checking if payer who is associaetd with source account but does not have pay role can be inserted. It cannot. Trigger verify_payer works :)
@@ -65,13 +68,25 @@ DELETE FROM statements WHERE statement_id = 10;
 /*Insert statement_confirmation data here*/
 INSERT INTO statement_confirmation VALUES(2, 8, FALSE);
 INSERT INTO statement_confirmation VALUES(1, 2, FALSE);
-INSERT INTO statement_confirmation VALUES(3, 2, TRUE);
+INSERT INTO statement_confirmation VALUES(4, 2, TRUE);
+INSERT INTO statement_confirmation VALUES(9, 1, TRUE);
+
+INSERT INTO statement_confirmation VALUES(4, 1, TRUE);
+
+INSERT INTO statement_confirmation VALUES(8, 2, FALSE);
 
 /*Insert statement_signer Data Here*/
 --need to add triggers functions contraints that check if client id is asscoiated with the statemnts source account and has sign role
-INSERT INTO statement_signer VALUES(2, 8, FALSE);
-INSERT INTO statement_signer VALUES(2, 1, FALSE);
-INSERT INTO statement_signer VALUES(2, 9, FALSE);
+INSERT INTO statement_signer VALUES(1, 1, TRUE);
+INSERT INTO statement_signer VALUES(5, 1, TRUE);
+INSERT INTO statement_signer VALUES(2, 2, TRUE);
+INSERT INTO statement_signer VALUES(8, 1, TRUE);
+INSERT INTO statement_signer VALUES(1, 1, TRUE);
+INSERT INTO statement_signer VALUES(2, 8, TRUE);
+INSERT INTO statement_signer VALUES(2, 1, TRUE);
+INSERT INTO statement_signer VALUES(2, 9, TRUE);
+
+
 
 --testing if invalid signer can be inserted. it cannot. trigger 'verify_signer_trigger' works :D
 INSERT INTO statement_signer VALUES(20, 8, TRUE);
@@ -86,23 +101,47 @@ INSERT INTO transactions(statement_id, amount, transaction_type, transaction_to)
 INSERT INTO transactions(statement_id, amount, transaction_type, transaction_to) VALUES(2, 101, 'deposit', 4);
 INSERT INTO transactions(statement_id, amount, transaction_type, transaction_to) VALUES(2, 10, 'deposit', 4);
 INSERT INTO transactions(statement_id, amount, transaction_type, transaction_to) VALUES(2, 100, 'withdrawal', 4);
+INSERT INTO transactions(statement_id, amount, transaction_type, transaction_to) VALUES(3, 100, 'withdrawal', 6);
+INSERT INTO transactions(statement_id, amount, transaction_type, transaction_to) VALUES(3, 1002, 'deposit', 6);
 
 --testing if trasaction can be added if statement is confirmed. It cannot be added. Trigger works :D
 INSERT INTO transactions(statement_id, amount, transaction_type, transaction_to) VALUES(4, 100, 'withdrawal', 4);
 
 UPDATE statements
-SET confirmed = FALSE
-WHERE statements.statement_id = 20; 
+SET total_amount = 2
+WHERE statements.statement_id = 5; 
+
+UPDATE statement_signer
+SET sign = TRUE
+WHERE statement_signer.statement_id = 8 AND statement_signer.signer_id = 1; 
+
+UPDATE statement_confirmation
+SET payer_id = 1
+WHERE statement_id = 1; 
+
+DELETE FROM statement_signer
+WHERE statement_signer.statement_id = 2;
+
+UPDATE statement_confirmation
+SET confirmed = TRUE
+WHERE statement_confirmation.statement_id = 1; 
 
 DELETE FROM transactions
 WHERE statement_id = 1;
 
 DELETE FROM statements
-WHERE statement_id=20;
+WHERE statement_id=5;
+
+DELETE FROM statement_confirmation
+WHERE statement_id=9;
+
 SELECT * FROM statements;
+SELECT * FROM client_account;
+SELECT * FROM statement_confirmation;
 SELECT * FROM statement_signer;
 SELECT * FROM transactions;
 SELECT * FROM client;
+SELECT * FROM account;
 
 DELETE FROM statement_signer;
 
